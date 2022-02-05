2e0c1247f84852282c5b3a8d45476eb963af5ef6
felipe.juaneda <felipejua9@gmail.com>
2022-02-05 16:23:43 -0300

    mejora

--- js/main.js
+++ js/main.js
@@ -12,15 +12,24 @@ class Producto{
         return "El " + this.local + " cuesta " + this.precio + " pesos POR MES " + "y cuenta con un espacio de " + this.espacio;
     }
 }
-
+//Declarar un array vacio
 const productos= [];
+
+//Con push agregar objetos
 productos.push(new Producto("Local 1", 5000,"3m x 4m"));
 productos.push(new Producto("Local 2", 7000, "5,5m x 5m"));
 productos.push(new Producto("Local 3", 8000, "6m x 6m"));
 productos.push(new Producto("Local 4", 10000, "8 x 7m"));
 
 
-for (const producto of productos) {
+/* for (const producto of productos) {
     alert("El " + producto.local + " cuesta " + producto.precio+ " pesos POR MES" + "\n"+ " Y cuenta con un espacio de " + producto.espacio)
+} */
+let sioNo = prompt('¿Deseas saber locales disponibles? Responde con si o no')
+if (sioNo=="si") {
+    for (const producto of productos) {
+        alert("El " + producto.local + " cuesta " + producto.precio+ " pesos POR MES" + "\n"+ " Y cuenta con un espacio de " + producto.espacio)
+    }
+} else {
+    alert("No hay problema!")
 }
-
